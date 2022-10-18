5139e6b845e38e8f19b48693b85cb171442d439d
Josphat32 <mwangijosphat2030@gmail.com>
2022-10-18 08:13:23 -0700

    print customs

--- main.h
+++ main.h
@@ -24,7 +24,18 @@ int get_flag(char s, flags_t *f);
 int _putchar(char c);
 int _puts(char *str);
 
-/* get_print */	
+/* get_print */
 int (*get_print(char s))(va_list, flags_t *);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
 
 #endif  /*MAIN_H */
