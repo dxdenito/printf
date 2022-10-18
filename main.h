3a4c667c2ceb9c3dd9b8cd0e9d1f8c8d20ce3b6d
Josphat32 <mwangijosphat2030@gmail.com>
2022-10-18 07:36:13 -0700

    set flags

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/* _printf */
 int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
 
 #endif  /*MAIN_H */
