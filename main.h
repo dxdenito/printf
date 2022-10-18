968a8384a34d87942a7f1a01a58f6652cdfed61c
Josphat32 <mwangijosphat2030@gmail.com>
2022-10-18 07:51:02 -0700

    get print function

--- main.h
+++ main.h
@@ -20,4 +20,11 @@ int (*get_print(char s))(va_list, flags_t *);
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* get_print */	
+int (*get_print(char s))(va_list, flags_t *);
+
 #endif  /*MAIN_H */
