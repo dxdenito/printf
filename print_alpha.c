1ece285d7f914bed14fbfaf9e493ca34f354bdbe
dxdenito <denixkaranjah@gmail.com>
2022-10-19 10:03:49 +0300

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -2,7 +2,7 @@
 
 /**
  * print_string - loops through a string and prints
- * every character
+ * every character.
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
